effc40db36333c021f474cd663d59f4d83418428
nvamshik <vnallamothu@quadyster.com>
2017-08-14 19:33:57 -0500

    Final Commit

--- py_aws_create_pro/py_aws_create_pro.py
+++ py_aws_create_pro/py_aws_create_pro.py
@@ -2,16 +2,19 @@ import boto3
 
 
 def main():
-    py_aws_create_pro('InstanceSecurityGroup', 'Enable SSH access',
-                      'tcp', '22', '22', '0.0.0.0/0', 'vpc-8fbd60e9')
+    create_sg(sg_name='InstanceSecurityGroup5', sg_desc='Enable SSH access', sg_ingress_IpProtocol='tcp',
+              sg_ingress_FromPort=22, sg_ingress_ToPort=22, sg_ingress_CidrIp='0.0.0.0/0', VpcId='vpc-8fbd60e9')
 
 
-def py_aws_create_pro(sg_name, sg_desc, sg_ingress_IpProtocol, sg_ingress_FromPort, sg_ingress_ToPort, sg_ingress_CidrIp, VpcId):
-    ec2_client.create_security_group(sg_name, sg_desc, sg_ingress_IpProtocol,
-                                     sg_ingress_FromPort, sg_ingress_ToPort, sg_ingress_CidrIp, VpcId)
+ec2 = boto3.client('ec2')
 
 
-ec2_client = boto3.client("ec2")
+def create_sg(**kwargs):
+    response = ec2.create_security_group(
+        GroupName=kwargs['sg_name'], Description=kwargs['sg_desc'], VpcId=kwargs['VpcId'])
+    ec2.authorize_security_group_ingress(GroupId=response['GroupId'], IpProtocol=kwargs['sg_ingress_IpProtocol'], CidrIp=kwargs["sg_ingress_CidrIp"], FromPort=kwargs["sg_ingress_FromPort"],
+                                         ToPort=kwargs['sg_ingress_ToPort'])
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
